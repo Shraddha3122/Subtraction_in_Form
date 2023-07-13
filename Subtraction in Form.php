9adca784b1b5ca0bba1f1887cb067e1567f700ae
Shraddha Hukkeri <113444373+Shraddha3122@users.noreply.github.com>
2023-07-13 09:13:04 +0530

    PHP Added

---
+++ Subtraction in Form.php
@@ -0,0 +1,20 @@
+<html>  
+<body>  
+<form method="post">  
+Enter First Number:  
+<input type="number" name="number1" /><br><br>  
+Enter Second Number:  
+<input type="number" name="number2" /><br><br>  
+<input  type="submit" name="submit" value="Subtract">  
+</form>  
+<?php  
+    if(isset($_POST['submit']))  
+    {  
+        $number1 = $_POST['number1'];  
+        $number2 = $_POST['number2'];  
+        $sum =  $number1-$number2;   
+        echo "The difference of $number1 and $number2 is: ".$sum;  
+    }  
+?>  
+</body>  
+</html>  
